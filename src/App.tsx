ec744c08525b241d7585abd2079b760022dfc316
Kaike <kaikedias98@gmail.com>
2026-04-19 21:50:59 -0300

    feat: add header to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Wizard from "@/features/wizard/wizard"
+import Header from "./components/header"
 
 export function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
+      <Header />
       <Wizard />
     </div>
   )
